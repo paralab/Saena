aeee6ebf47f99459107907d89dfc6bd559b35a6f
Majid Rasouli <u1013493@utah.edu>
2016-11-09 05:41:59 -0700

    new data from UoF

---
+++ data/LFAT5/LFAT5 (2)/LFAT5_B.rb
@@ -0,0 +1,6 @@
+Oberwolfach/LFAT5_B                                                    |1440B   
+             2             1             1             0
+pra                       14             1             1             0
+(40I2)          (26I3)                              
+ 1 2
+  8
