2b36068f7f6b4d7d74ebeb65204d907a470b282c
Majid Rasouli <u1013493@utah.edu>
2016-12-16 02:06:16 -0700

    ready for experiments

--- src/coomatrix.cpp
+++ src/coomatrix.cpp
@@ -1,11 +1,11 @@
 #include <fstream>
 #include <algorithm>
 #include <sys/stat.h>
-#include "stdlib.h"
-
 #include "coomatrix.h"
+//#include <math.h>
 #include "mpi.h"
 
+// binary search tree using the lower bound
 template <class T>
 T lower_bound2(T *left, T *right, T val) {
     T* first = left;
@@ -26,7 +26,7 @@ T lower_bound2(T *left, T *right, T val) {
 }
 
 
-COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
+COOMatrix::COOMatrix(char* Aname, long Mbig) {
 
     int nprocs;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
@@ -34,7 +34,7 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     struct stat st;
-    stat(filePath, &st);
+    stat(Aname, &st);
     // 2*sizeof(long)+sizeof(double) = 24
     long nnz_g = st.st_size/24;
 
@@ -42,38 +42,56 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
     MPI_File fh;
     MPI_Offset offset;
 
-    MPI_File_open(MPI_COMM_WORLD, filePath2, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+    MPI_File_open(MPI_COMM_WORLD, Aname, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 
-    long first_nnz_l = nnz_g / nprocs; //local nnz
-    offset = rank * first_nnz_l * 24;
-    long data[3*first_nnz_l];
-    MPI_File_read_at(fh, offset, data, 3*first_nnz_l, MPI_UNSIGNED_LONG, &status);
+    long initial_nnz_l = long(floor(1.0*nnz_g/nprocs)); // initial local nnz
+    if (rank==nprocs-1)
+        initial_nnz_l = nnz_g - (nprocs-1)*initial_nnz_l;
+
+    //offset = rank * initial_nnz_l * 24; // row index(long=8) + column index(long=8) + value(double=8) = 24
+    // the offset for the last process will be wrong if you use the above formula, because initial_nnz_l of the last process will be used, instead of the initial_nnz_l of the other processes.
+    offset = rank * long(floor(1.0*nnz_g/nprocs)) * 24; // row index(long=8) + column index(long=8) + value(double=8) = 24
+    long data[3*initial_nnz_l];
+    MPI_File_read_at(fh, offset, data, 3*initial_nnz_l, MPI_UNSIGNED_LONG, &status);
 
     int count;
     MPI_Get_count(&status, MPI_UNSIGNED_LONG, &count);
     //printf("process %d read %d lines of triples\n", rank, count);
     MPI_File_close(&fh);
 
-    //cout << "nnz_g = " << nnz_g << ", first_nnz_l = " << first_nnz_l << endl;
+    //cout << "nnz_g = " << nnz_g << ", initial_nnz_l = " << initial_nnz_l << endl;
 
 /*    if (rank==0){
         cout << "data:" << endl;
-        for (int i=0; i<3*first_nnz_l;i++){
+        for (int i=0; i<3*initial_nnz_l;i++){
             cout << data[i] << endl;
         }
     }*/
 
+    long n_buckets;
 
+    if (Mbig > nprocs*nprocs){
+        if (nprocs < 1000)
+            n_buckets = nprocs*nprocs;
+        else
+            n_buckets = 1000*nprocs;
+    }
+    else
+        n_buckets = Mbig;
 
-    long m = 18;
-    long n_buckets = nprocs*nprocs;
     // change this part to make it work with any number of processors, instead of just the factors of m
-    long splitOffset = m / n_buckets;
+    long splitOffset = long(floor(1.0*Mbig/n_buckets));
+
+    //long n_buckets = nprocs*nprocs;
+    // change this part to make it work with any number of processors, instead of just the factors of m
+    //long splitOffset = Mbig / n_buckets;
+
     long firstSplit[n_buckets+1];
 
-    for(long i=0; i<n_buckets+1; i++){
+    for(long i=0; i<n_buckets; i++){
         firstSplit[i] = i*splitOffset;
     }
+    firstSplit[n_buckets] = Mbig;
 
 /*    if(rank==0){
         cout << "firstSplit:" << endl;
@@ -86,12 +104,13 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
     long H_l[n_buckets];
     fill(&H_l[0], &H_l[n_buckets], 0);
 
-    long* low2;
-    long temp;
-    for(long i=0; i<first_nnz_l; i++)
+    for(long i=0; i<initial_nnz_l; i++){
         H_l[lower_bound2(&firstSplit[0], &firstSplit[n_buckets], data[3*i])]++;
+        //cout << "data = " << data[3*i] << ", cpu owner = " << lower_bound2(&firstSplit[0], &firstSplit[n_buckets], data[3*i]) << endl;
+    }
 
 /*    if (rank==0){
+        cout << "initial_nnz_l = " << initial_nnz_l << endl;
         cout << "local histogram:" << endl;
         for(long i=0; i<n_buckets; i++)
             cout << H_l[i] << endl;
@@ -112,13 +131,11 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
     for (long i=1; i<n_buckets; i++)
         H_g_scan[i] = H_g[i] + H_g_scan[i-1];
 
-/*
-    if (rank==0){
+/*    if (rank==0){
         cout << "scan of global histogram:" << endl;
         for(long i=0; i<n_buckets; i++)
             cout << H_g_scan[i] << endl;
-    }
-*/
+    }*/
 
     long procNum=0;
     //long split[nprocs+1];
@@ -132,28 +149,18 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
         }
     }
 
-/*
-    if (rank==0){
+    split[nprocs] = Mbig;
+
+/*    if (rank==0){
         cout << "split:" << endl;
         for(long i=0; i<nprocs+1; i++)
             cout << split[i] << endl;
-    }
-*/
-
-/*    if(rank==3){
-        procNum=2;
-        long nnzTest = 0;
-        for (long i=0; i<first_nnz_l; i++){
-            if( data[3*i]>=split[procNum] && data[3*i]<split[procNum+1] )
-                nnzTest++;
-        }
-        cout << "nnzTest = " << nnzTest << endl;
     }*/
 
     long tempIndex;
     int sendSizeArray[nprocs];
     fill(&sendSizeArray[0], &sendSizeArray[nprocs], 0);
-    for (long i=0; i<first_nnz_l; i++){
+    for (long i=0; i<initial_nnz_l; i++){
         tempIndex = lower_bound2(&split[0], &split[nprocs+1], data[3*i]);
         sendSizeArray[tempIndex]++;
     }
@@ -163,10 +170,6 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
         for(long i=0;i<nprocs;i++)
             cout << sendSizeArray[i] << endl;
     }*/
-
-/*    cout << endl << "sendSizeArray:" << "rank=" << rank << endl;
-    for(long j=0;j<nprocs;j++)
-        cout  << sendSizeArray[j] << endl;*/
 
     int recvSizeArray[nprocs];
     MPI_Alltoall(sendSizeArray, 1, MPI_INT, recvSizeArray, 1, MPI_INT, MPI_COMM_WORLD);
@@ -201,14 +204,13 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
 
     long procOwner;
     long bufTemp;
-    long sendBufI[first_nnz_l];
-    long sendBufJ[first_nnz_l];
-    long sendBufV[first_nnz_l];
+    long sendBufI[initial_nnz_l];
+    long sendBufJ[initial_nnz_l];
+    long sendBufV[initial_nnz_l];
     long sIndex[nprocs];
     fill(&sIndex[0], &sIndex[nprocs], 0);
 
-    for (long i=0; i<first_nnz_l; i++){
-        procOwner = lower_bound2(&split[0], &split[nprocs+1], data[3*i]);
+    for (long i=0; i<initial_nnz_l; i++){
         procOwner = lower_bound2(&split[0], &split[nprocs+1], data[3*i]);
         bufTemp = sOffset[procOwner]+sIndex[procOwner];
         sendBufI[bufTemp] = data[3*i];
@@ -217,73 +219,89 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
         sIndex[procOwner]++;
     }
 
-/*    if (rank==0){
-        cout << "sendBufI:" << endl;
-        for (long i=0; i<first_nnz_l; i++)
-            cout << sendBufI[i] << endl;
+/*    if (rank==1){
+        cout << "sendBufJ:" << endl;
+        for (long i=0; i<initial_nnz_l; i++)
+            cout << sendBufJ[i] << endl;
     }*/
 
     nnz_l = rOffset[nprocs-1] + recvSizeArray[nprocs-1];
-    //long row[nnz_l];
-    //long col[nnz_l];
-    //double values[nnz_l];
+    //cout << "nnz_l = " << nnz_l << endl;
 
-    values = (double*) malloc(sizeof(double) * nnz_l);
-    row = (long*) malloc(sizeof(long) * nnz_l);
-    col = (long*) malloc(sizeof(long) * nnz_l);
+    values.resize(nnz_l);
+    row.resize(nnz_l);
+    col.resize(nnz_l);
 
-    MPI_Alltoallv(sendBufI, sendSizeArray, sOffset, MPI_LONG, row, recvSizeArray, rOffset, MPI_LONG, MPI_COMM_WORLD);
-    MPI_Alltoallv(sendBufJ, sendSizeArray, sOffset, MPI_LONG, col, recvSizeArray, rOffset, MPI_LONG, MPI_COMM_WORLD);
-    MPI_Alltoallv(sendBufV, sendSizeArray, sOffset, MPI_DOUBLE, values, recvSizeArray, rOffset, MPI_DOUBLE, MPI_COMM_WORLD);
+    double* valuesP = &(*(values.begin()));
+    long* rowP = &(*(row.begin()));
+    long* colP = &(*(col.begin()));
+
+    MPI_Alltoallv(sendBufI, sendSizeArray, sOffset, MPI_LONG, rowP, recvSizeArray, rOffset, MPI_LONG, MPI_COMM_WORLD);
+    MPI_Alltoallv(sendBufJ, sendSizeArray, sOffset, MPI_LONG, colP, recvSizeArray, rOffset, MPI_LONG, MPI_COMM_WORLD);
+    MPI_Alltoallv(sendBufV, sendSizeArray, sOffset, MPI_DOUBLE, valuesP, recvSizeArray, rOffset, MPI_DOUBLE, MPI_COMM_WORLD);
 
     // setting the number of rows for processor
     M = split[rank+1] - split[rank];
 
- /*   if (rank==0){
-        cout << "M = " << M << endl;
-        cout << "nnz_l = " << nnz_l << endl;
-        valprint();
+/*    if (rank==3){
+        colprint();
+        //cout << "M = " << M << endl;
+        //cout << "nnz_l = " << nnz_l << endl;
+        //valprint();
+        //print();
     }*/
 
-    //M = m/nprocs;
-    //N = Nbig;
-
     vElement = (long*) malloc(sizeof(long) * nnz_l);
-    vElementRep = (long*) malloc(sizeof(long) * nnz_l);
+    vElementRep = (long*) malloc(sizeof(long) * nnz_l); //the size of this, which is nnz_l, will be less, exactly: vElementSize
     vElementSize = 0;
     recvCount = (int*)malloc(sizeof(long)*nprocs);
-    std::fill(recvCount, recvCount + nprocs, 0.);
-    procNum = 0;
+    std::fill(recvCount, recvCount + nprocs, 0);
+    std::fill(vElementRep, vElementRep + nnz_l, 0);
 
     vElement[0] = col[0];
     vElementSize = 1;
     vElementRep[0] = 1;
-    //recvCount[findProcess(col[0], procNum, nprocs)] = 1;
-    recvCount[lower_bound2(&split[0], &split[n_buckets], col[0])] = 1;
-
+    recvCount[lower_bound2(&split[0], &split[nprocs], col[0])] = 1;
+    bool temp;
     for (long i = 1; i < nnz_l; i++) {
-        if(col[i] == col[i-1]){
-            vElementRep[vElementSize-1] = vElementRep[vElementSize-1] + 1;
+
+        temp = (col[i] != col[i-1]);
+
+        vElementRep[vElementSize-1] += (1-temp);        // vElementRep[vElementSize-1]++;
+
+        vElement[vElementSize] = col[i];
+        vElementRep[vElementSize] += temp;
+        vElementSize += temp;
+
+        procNum = lower_bound2(&split[0], &split[nprocs], col[i]);
+        recvCount[procNum] += temp;
+
+        // following "if statement" is changed to the above part. Computing the temp value is the same as checking the if statement. Which one is better?
+
+/*        if(col[i] == col[i-1]){
+            vElementRep[vElementSize-1]++;
         }else{
             vElement[vElementSize] = col[i];
             vElementRep[vElementSize] = 1;
             vElementSize++;
 
             //procNum = findProcess(col[i], procNum, nprocs);
-            procNum = lower_bound2(&split[0], &split[n_buckets], col[i]);
+            procNum = lower_bound2(&split[0], &split[nprocs], col[i]);
+            //cout << "rank = " << rank << ", nnz_l = " << nnz_l << ", col[i] = " << col[i] << ", procNum = " << procNum << endl;
             recvCount[procNum]++;
             //cout << "recvCount[procNum] = " << recvCount[procNum] << endl;
-        }
+        }*/
     }
 
-/*    if (rank==0){
+/*    if (rank==3){
+        vElementprint();
+        //vElementRepprint();
         cout << "recvCount:" << endl;
         for(int i=0; i<nprocs; i++)
             cout << recvCount[i] << endl;
     }*/
 
     vIndexCount = (int*)malloc(sizeof(int)*nprocs);
-
     MPI_Alltoall(recvCount, 1, MPI_INT, vIndexCount, 1, MPI_INT, MPI_COMM_WORLD);
 
 /*    if (rank==1){
@@ -292,18 +310,18 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
             cout << vIndexCount[i] << endl;
     }*/
 
-    int vIndexSize = 0;
+/*    int vIndexSize = 0;
     for (int i=0; i<nprocs; i++)
-        vIndexSize += vIndexCount[i];
+        vIndexSize += vIndexCount[i];*/
 
-/*
     long* vBuf = (long*)malloc(sizeof(long)*nnz_l);
     for (long i=0; i<vElementSize; i++)
         vBuf[i] = vElement[i]%M;
-*/
 
-    int* vdispls = (int*)malloc(sizeof(int)*nprocs);
-    int* rdispls = (int*)malloc(sizeof(int)*nprocs);
+    vdispls.resize(nprocs);
+    rdispls.resize(nprocs);
+    //int* vdispls = (int*)malloc(sizeof(int)*nprocs);
+    //int* rdispls = (int*)malloc(sizeof(int)*nprocs);
     vdispls[0] = 0;
     rdispls[0] = 0;
 
@@ -311,20 +329,16 @@ COOMatrix::COOMatrix(const char* filePath, char* filePath2) {
         vdispls[i] = vdispls[i-1] + vIndexCount[i-1];
         rdispls[i] = rdispls[i-1] + recvCount[i-1];
     }
+    vIndexSize = vdispls[nprocs-1] + vIndexCount[nprocs-1];
+    recvSize = rdispls[nprocs-1] + recvCount[nprocs-1];
 
     vIndex = (long*)malloc(sizeof(long)*vIndexSize);
-    MPI_Alltoallv(vElement, recvCount, rdispls, MPI_LONG, vIndex, vIndexCount, vdispls, MPI_LONG, MPI_COMM_WORLD);
+    MPI_Alltoallv(vElement, recvCount, &*(rdispls.begin()), MPI_LONG, vIndex, vIndexCount, &*(vdispls.begin()), MPI_LONG, MPI_COMM_WORLD);
 
-    free(vdispls);
-    free(rdispls);
-    //free(vBuf);
-
+    free(vBuf);
 }
 
 COOMatrix::~COOMatrix() {
-    free(values);
-    free(row);
-    free(col);
     free(vElement);
     free(vElementRep);
     free(recvCount);
@@ -338,37 +352,13 @@ void COOMatrix::matvec(double* v, double* w){
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-/*    long vIndexSize = 0;
-    for (int i=0; i<nprocs; i++)
-        vIndexSize += vIndexCount[i];*/
-
-    int vdispls[nprocs];
-    int rdispls[nprocs];
-    vdispls[0] = 0;
-    rdispls[0] = 0;
-    for (int i=1; i<nprocs; i++){
-        vdispls[i] = vdispls[i-1] + vIndexCount[i-1];
-        rdispls[i] = rdispls[i-1] + recvCount[i-1];
-    }
-
-    long vIndexSize = vdispls[nprocs-1] + vIndexCount[nprocs-1];
-    long recvSize = rdispls[nprocs-1] + recvCount[nprocs-1];
-
     double *vSend = (double*)malloc(sizeof(double) * vIndexSize);
     for(long i=0;i<vIndexSize;i++){
-        //if (rank==1) cout << "vIndex[i]%M = " << vIndex[i]%M << endl;
-        vSend[i] = v[vIndex[i]%M];
+        vSend[i] = v[( vIndex[i]-split[rank] )];
     }
 
     double* vValuesCompressed = (double*) malloc(sizeof(double) * recvSize);
-    MPI_Alltoallv(vSend, vIndexCount, vdispls, MPI_DOUBLE, vValuesCompressed, recvCount, rdispls, MPI_DOUBLE, MPI_COMM_WORLD);
-
-/*    if (rank==0){
-        cout << "vValuesCompressed:" << endl;
-        for(long int i=0;i<recvSize;i++) {
-            cout << vValuesCompressed[i] << endl;
-        }
-    }*/
+    MPI_Alltoallv(vSend, vIndexCount, &*(vdispls.begin()), MPI_DOUBLE, vValuesCompressed, recvCount, &*(rdispls.begin()), MPI_DOUBLE, MPI_COMM_WORLD);
 
     long iter = 0;
     double *vValues = (double *) malloc(sizeof(double) * nnz_l);
@@ -378,13 +368,6 @@ void COOMatrix::matvec(double* v, double* w){
             iter++;
         }
     }
-
-/*    if (rank==0){
-        cout << "vValues:" << endl;
-        for(long int i=0;i<nnz_l;i++) {
-            cout << vValues[i] << endl;
-        }
-    }*/
 
     fill(&w[0], &w[M], 0);
     // the following and the above loops can be combined to have only one nested for loop.
@@ -396,53 +379,51 @@ void COOMatrix::matvec(double* v, double* w){
 
     free(vSend);
     free(vValuesCompressed);
-    //free(vValues);
 }
 
 void COOMatrix::valprint(){
     cout << "val:" << endl;
-    for(long int i=0;i<nnz_l;i++) {
+    for(long i=0;i<nnz_l;i++) {
         cout << values[i] << endl;
     }
 }
 
 void COOMatrix::rowprint(){
     cout << "row:" << endl;
-    for(long int i=0;i<nnz_l;i++) {
+    for(long i=0;i<nnz_l;i++) {
         cout << row[i] << endl;
     }
 }
 
 void COOMatrix::colprint(){
     cout << endl << "col:" << endl;
-    for(long int i=0;i<nnz_l;i++) {
+    for(long i=0;i<nnz_l;i++) {
         cout << col[i] << endl;
     }
 }
 
 void COOMatrix::vElementprint(){
     cout << endl << "vElement:" << endl;
-    for(long int i=0;i<vElementSize;i++) {
+    for(long i=0;i<vElementSize;i++) {
         cout << vElement[i] << endl;
     }
 }
 
 void COOMatrix::vElementRepprint(){
     cout << endl << "vElementRep:" << endl;
-    for(long int i=0;i<vElementSize;i++) {
+    for(long i=0;i<vElementSize;i++) {
         cout << vElementRep[i] << endl;
     }
 }
 
 void COOMatrix::print(){
     cout << endl << "triple:" << endl;
-    for(long int i=0;i<nnz_l;i++) {
+    for(long i=0;i<nnz_l;i++) {
         cout << "(" << row[i] << " , " << col[i] << " , " << values[i] << ")" << endl;
     }
 }
 
-/*
-int COOMatrix::findProcess(long a, int procNum, int p) {
+/*int COOMatrix::findProcess(long a, int procNum, int p) {
     while(procNum < p){
         if (a >= procNum*M && a < (procNum+1)*M)
             return procNum;
--- src/main.cpp
+++ src/main.cpp
@@ -4,59 +4,129 @@
 #include "coomatrix.h"
 //#include "cscmatrix.h"
 //#include "csrmatrix.h"
-#include <time.h>
+#include <sys/time.h>
 #include "mpi.h"
+
+#define ITERATIONS 100
 
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]){
 
-    MPI_Init(NULL, NULL);
-    int p;
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
-    int rank;
+    MPI_Init(&argc, &argv);
+    int nprocs, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    const char* filePath = "/home/abaris/Dropbox/Projects/Saena/data/ch3-3-b1/ch3-3-b1.bin";
-    char* filePath2 = "/home/abaris/Dropbox/Projects/Saena/data/ch3-3-b1/ch3-3-b1.bin";
+    //const char* Aname = "/home/abaris/Dropbox/Projects/Saena/data/ch3-3-b1/ch3-3-b1.bin";
+    //char* Aname2 = "/home/abaris/Dropbox/Projects/Saena/data/ch3-3-b1/ch3-3-b1.bin";
 
-    COOMatrix B (filePath, filePath2);
+    if(argc < 4)
+    {
+        if(rank == 0)
+        {
+            cout << "Usage: ./Saena <MatrixA> <vecX> <#rows of A>" << endl;
+            cout << "<MatrixA> is absolute address, and files should be in triples format" << endl;
+        }
+        MPI_Finalize();
+        return -1;
+    }
 
-    srand (time(NULL));
+    char* Aname(argv[1]);
+    long Mbig = stol(argv[3]);
 
-    long Mbig = 18;
-    //long Nbig = 9;
+/*    if (Mbig%(nprocs*nprocs) != 0){
+        if (rank==0)
+            cout << "This code only works when the number of rows are divisible by (number of prcessors)^2" << endl;
+
+        MPI_Finalize();
+        return -1;
+    }*/
+
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double t1 = MPI_Wtime();
+    COOMatrix B (Aname, Mbig);
+    MPI_Barrier(MPI_COMM_WORLD);
+    double t2 = MPI_Wtime();
+
+    if (rank==0)
+        cout << "Setup in Saena took " << t2 - t1 << " seconds!" << endl;
+
+    char* Vname(argv[2]);
+    MPI_Status status;
+    MPI_File fh;
+    MPI_Offset offset;
+
+    MPI_File_open(MPI_COMM_WORLD, Vname, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+
     double* v = (double*) malloc(sizeof(double) * B.M);
-    double* w = (double*) malloc(sizeof(double) * B.M);
 
+    // vector should have the following format: first line shows the value in row 0, second line shows the value in row 1
+    offset = B.split[rank] * 8; // value(double=8)
+    MPI_File_read_at(fh, offset, v, B.M, MPI_UNSIGNED_LONG, &status);
+
+    int count;
+    MPI_Get_count(&status, MPI_UNSIGNED_LONG, &count);
+    //printf("process %d read %d lines of triples\n", rank, count);
+    MPI_File_close(&fh);
+
+/*    srand (time(NULL));
     for (long i=0; i<B.M; i++){
         v[i] = rand();
+    }*/
+
+    double* w = (double*) malloc(sizeof(double) * B.M);
+
+    // warming up
+    for(int i=0; i<ITERATIONS; i++){
+        B.matvec(v, w);
+        v = w;
     }
 
-    B.matvec(v, w);
-
-/*    int a;
-    if (rank == 0) {
-        cout << endl<< "result should be: " << endl;
-        for(unsigned int i=0;i<N;i++){
-            a = 0;
-            for(unsigned int j=0;j<N;j++)
-                a += (i+j) * j;
-            cout << a << endl;
-        }
+    // timing matvec
+    MPI_Barrier(MPI_COMM_WORLD);
+    t1 = MPI_Wtime();
+    for(int i=0; i<ITERATIONS; i++){
+        B.matvec(v, w);
+        v = w;
     }
+    MPI_Barrier(MPI_COMM_WORLD);
+    t2 = MPI_Wtime();
+    //end of timing matvec
 
-    B.matvec(v, w, M, N);
+    if (rank==0)
+        cout << "Matvec in Saena took " << (t2 - t1)/ITERATIONS << " seconds!" << endl;
 
+
+/*
     if (rank == 0) cout << endl << "result of matvec: " << endl;
     for(unsigned int i=0;i<M;i++)
         cout << w[i] << endl;
 */
-    free(v);
-    free(w);
 
-    //free(filePath);
-    //free(filePath2);
+    // write the result of the matvec
+    // txt file
+    char* outFileNameTxt = "matvec_result_saena.bin";
+
+    MPI_Status status2;
+    MPI_File fh2;
+    MPI_Offset offset2;
+
+    MPI_File_open(MPI_COMM_WORLD, outFileNameTxt, MPI_MODE_CREATE| MPI_MODE_WRONLY, MPI_INFO_NULL, &fh2);
+
+    offset2 = B.split[rank] * 8; // value(double=8)
+    MPI_File_write_at(fh2, offset2, v, B.M, MPI_UNSIGNED_LONG, &status2);
+
+    int count2;
+    MPI_Get_count(&status2, MPI_UNSIGNED_LONG, &count2);
+    //printf("process %d wrote %d lines of triples\n", rank, count2);
+    MPI_File_close(&fh2);
+
+    free(v);
+    //free(w);
+    //free(Aname);
+    //free(Aname2);
 
     MPI_Finalize();
     return 0;
