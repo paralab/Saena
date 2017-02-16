7d9e19fa5467a3bcf6d891caf4bc5237a2a1ff9e
Majid Rasouli <u1013493@utah.edu>
2017-02-15 22:23:20 -0700

    add Jacobi

--- include/coomatrix.h
+++ include/coomatrix.h
@@ -3,9 +3,11 @@
 
 #include <iostream>
 #include <vector>
+#include "mpi.h"
+
 using namespace std;
 
-#define matrixTol 1.e-6
+#define MATRIX_TOL 1.e-6
 /**
  * @author Majid
  * @breif Contains the basic structure to define coo matrices
@@ -14,8 +16,6 @@ using namespace std;
 class COOMatrix {
 
 private:
-    int *recvCount;
-    int *vIndexCount;
     long *vIndex;
     std::vector<int> splitOffset;
     std::vector<int> vdispls;
@@ -28,72 +28,59 @@ private:
     std::vector<int> recvProcCount;
     std::vector<int> sendProcRank;
     std::vector<int> sendProcCount;
-    std::vector<double> values_local;
-    std::vector<double> values_remote;
     std::vector<long> row_local;
     std::vector<long> row_remote;
-    std::vector<long> col_local;
-    std::vector<long> col_remote;
+
     long num_threads;
-//    int thread_id;
-    long* iter_local_array;
-    long* iter_remote_array;
-    int* indicesP;
-    int* indicesP_local;
-    int* indicesP_remote;
-    //long *vElement;
-    //std::vector<long> vElement_local;
+    unsigned int* iter_local_array;
+    unsigned int* iter_remote_array;
     std::vector<long> vElement_remote;
-    //long vElementSize;
-    //long vElementSize_local;
-    //long vElementSize_remote;
-    //long *vElementRep;
     std::vector<long> vElementRep_local;
     std::vector<long> vElementRep_remote;
-    std::vector<long> nnz_row_local;
-    std::vector<long> nnz_row_remote;
-    long nnz_l_local;
-    long nnz_l_remote;
+
+    unsigned int nnz_l_local;
+    unsigned int nnz_l_remote;
     long col_remote_size;
 
 public:
-    long M;
-    //long N;
+    unsigned int M;
+    unsigned int Mbig;
     unsigned long nnz_l;
-
     int nprocs, rank;
 
     std::vector<double> values;
     std::vector<long> row;
     std::vector<long> col;
-
     std::vector<long> split;
     double *vSend;
     double* vecValues;
+    std::vector<double> values_local;
+    std::vector<double> values_remote;
+    std::vector<long> col_local;
+    std::vector<long> col_remote;
+    std::vector<unsigned int> nnz_row_local;
+    std::vector<unsigned int> nnz_row_remote;
+    std::vector<double> invDiag;
 
+//    int* indicesP;
+    int* indicesP_local;
+    int* indicesP_remote;
+
+    // functions
+    void print();
     void matvec(double* v, double* w);
+    void jacobi(double* v, double* w);
+    void inverseDiag(double* x);
 
     double time[4];
     double totalTime=0;
-
-    // print functions
-    void valprint();
-    void rowprint();
-    void colprint();
-    //void vElementprint();
-    //void vElementprint_local();
-    void vElementprint_remote();
-    //void vElementRepprint();
-    void vElementRepprint_local();
-    void vElementRepprint_remote();
-    void print();
 
     //COOMatrix();
     /**
      * @param[in] Aname is the pointer to the matrix
      * @param[in] Mbig Number of rows in the matrix
      * */
-    COOMatrix(char* Aname, long Mbig);
+    COOMatrix(char* Aname, unsigned int Mbig);
     ~COOMatrix();
 
     // for sparsifying:
