4311b7a52d40bdfcc5c97d9c5dda029aedc1a07f
majidrp <u1013493@utah.edu>
2018-07-25 00:20:16 -0600

    huge update

---
+++ external/SuperLU_DIST_5.4.0/SRC/superlu_dist_config.h
@@ -0,0 +1,15 @@
+/* superlu_dist_config.h.in */
+
+/* Enable parmetis */
+#define HAVE_PARMETIS TRUE
+
+/* Enable CombBLAS */
+/* #undef HAVE_COMBBLAS */
+
+/* enable 64bit index mode */
+#define XSDK_INDEX_SIZE 64
+
+#if (XSDK_INDEX_SIZE == 64)
+#define _LONGINT 1
+#endif
+
