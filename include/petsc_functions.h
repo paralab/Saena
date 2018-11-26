d93a06da04843181b9cc8227e5ab40ff1bbae88a
majidrp <u1013493@utah.edu>
2018-11-26 00:47:41 -0700

    add new petsc functions to petsc_functions.h

--- include/petsc_functions.h
+++ include/petsc_functions.h
@@ -10,9 +10,11 @@ PetscErrorCode ComputeMatrix(KSP ksp, Mat J, Mat jac, void *ctx);
 
 PetscErrorCode ComputeRHS(KSP ksp,Vec b,void *ctx);
 
+int petsc_viewer(Mat &A);
 int petsc_viewer(saena_matrix *A);
-int petsc_viewer(saena_matrix *A);
-
+int petsc_prolong_matrix(prolong_matrix *P, Mat &B);
+int petsc_restrict_matrix(restrict_matrix *R, Mat &B);
+int petsc_saena_matrix(saena_matrix *A, Mat &B);
 int petsc_coarsen(restrict_matrix *R, saena_matrix *A, prolong_matrix *P);
 
 #endif //SAENA_PETSC_FUNCTIONS_H
