69550c4fcacdd4607a598844be8b6fa2b75f211f
majidrp <u1013493@utah.edu>
2019-01-09 15:00:22 -0700

    fix bracelet in fast_MM

--- src/saena_object_setup2.cpp
+++ src/saena_object_setup2.cpp
@@ -640,7 +640,7 @@ int saena_object::fast_mm_nnz(const cooEntry *A, const cooEntry *B, std::vector<
 
 
 // this version splits the matrices by the middle row and column.
-int saena_object::fast_mm(const cooEntry *A, const cooEntry *B, std::vector<cooEntry> &C,
+int saena_object::fast_mm_orig(const cooEntry *A, const cooEntry *B, std::vector<cooEntry> &C,
                                const nnz_t A_nnz, const nnz_t B_nnz,
                                const index_t A_row_size, const index_t A_row_offset, const index_t A_col_size, const index_t A_col_offset,
                                const index_t B_col_size, const index_t B_col_offset,
@@ -1384,6 +1384,11 @@ int saena_object::fast_mm(const cooEntry *A, const cooEntry *B, std::vector<cooE
 
 #ifdef __DEBUG1__
         if(rank==verbose_rank && verbose_matmat) printf("fast_mm: case 3: end \n");
+#endif
+    }
+
+#ifdef __DEBUG1__
+    if(rank==verbose_rank && verbose_matmat) printf("fast_mm: end \n\n");
 #endif
 
     return 0;
