e2b1f6ca36e8bad8b7b6edb3adac283e50102037
majidrp <u1013493@utah.edu>
2019-01-17 20:41:53 -0700

    minor

--- src/saena_object.cpp
+++ src/saena_object.cpp
@@ -323,7 +323,9 @@ int saena_object::coarsen(Grid *grid){
 //           rank, grid->A->M, grid->A->nnz_l, grid->A->nnz_g, grid->Ac.M, grid->Ac.nnz_l);
 #endif
 
-    if(!rank)
+    int rank1;
+    MPI_Comm_rank(grid->A->comm, &rank1);
+    if(!rank1)
         printf("rank = %d, Mbig = %u, M = %u, nnz_l = %lu, nnz_g = %lu \n",
                rank, grid->Ac.Mbig, grid->Ac.M, grid->Ac.nnz_l, grid->Ac.nnz_g);
 //    print_vector(grid->Ac.entry, 0, "grid->Ac.entry", grid->Ac.comm);
