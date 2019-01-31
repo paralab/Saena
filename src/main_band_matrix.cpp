f7dbca6fdf04fdee0b65ead2637cdac23aca324e
majidrp <u1013493@utah.edu>
2019-01-31 00:16:01 -0700

    change matmat_iter

--- src/main_band_matrix.cpp
+++ src/main_band_matrix.cpp
@@ -128,8 +128,8 @@ int main(int argc, char* argv[]){
     // *************************** matrix-matrix product ****************************
 
     double matmat_time = 0;
-    int matmat_iter_warmup = 0;
-    int matmat_iter = 1;
+    int matmat_iter_warmup = 2;
+    int matmat_iter = 5;
 
     saena::amg solver;
 //    saena::matrix C(comm);
