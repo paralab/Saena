196cbad65f3b6ca963c7f8e1d96d540749e1ba36
majidrp <u1013493@utah.edu>
2019-05-21 16:36:14 -0600

    change ITER_LAZY to 10

--- include/saena_object.h
+++ include/saena_object.h
@@ -24,7 +24,7 @@ typedef unsigned int  index_t;
 typedef unsigned long nnz_t;
 typedef double        value_t;
 
-#define ITER_LAZY 6
+#define ITER_LAZY 10
 
 class strength_matrix;
 class saena_matrix;
