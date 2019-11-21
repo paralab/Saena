e18f12d1033291ba5abb84a5efb257fdab88aa7a
majidrp <u1013493@utah.edu>
2019-11-21 14:30:24 -0700

    update the test file

--- test/test_parallel_range_matmat_random.py
+++ test/test_parallel_range_matmat_random.py
@@ -13,15 +13,13 @@ import subprocess
 #args = "bin/bar -c somefile.xml -d text.txt -r aString -f anotherString".split()
 # args = "./Saena 3 3 3"
 
-
-
-start_range = int(float(sys.argv[3]) * 100);
-end_range   = int(float(sys.argv[4]) * 100);
-step_size   = int(float(sys.argv[5]) * 100);
+start_range = int(float(sys.argv[3]) * 1000);
+end_range   = int(float(sys.argv[4]) * 1000);
+step_size   = int(float(sys.argv[5]) * 1000);
 
 for x in range(start_range, end_range, step_size):
     # print str(float(x)/100)
-    args = ("mpirun", "-np", sys.argv[1], "./Saena_matmat", sys.argv[2], str(float(x)/100))
+    args = ("mpirun", "-np", sys.argv[1], "./Saena_matmat", sys.argv[2], str(float(x)/1000))
     popen = subprocess.Popen(args, stdout=subprocess.PIPE)
     popen.wait()
     output = popen.stdout.read()
