09f215bb14d26c8cbc3011f2335f30cb4a6ed785
majidrp <u1013493@utah.edu>
2020-04-07 14:01:53 -0600

    add basic commands in pcoarsen()

--- src/saena_object_setup3.cpp
+++ src/saena_object_setup3.cpp
@@ -1,6 +1,22 @@
 #include <saena_object.h>
+#include "saena_matrix.h"
+#include "strength_matrix.h"
+#include "prolong_matrix.h"
+#include "restrict_matrix.h"
+#include "grid.h"
+#include "aux_functions.h"
 
 int saena_object::pcoarsen(Grid *grid){
+
+    saena_matrix    *A  = grid->A;
+    prolong_matrix  *P  = &grid->P;
+    restrict_matrix *R  = &grid->R;
+    saena_matrix    *Ac = &grid->Ac;
+
+    MPI_Comm comm = A->comm;
+    int nprocs, rank;
+    MPI_Comm_size(comm, &nprocs);
+    MPI_Comm_rank(comm, &rank);
 
     return 0;
 }
