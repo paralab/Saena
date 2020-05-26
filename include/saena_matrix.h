2096726028800bf2e02b18808a2fbba74d62aa0e
Majid Rasouli <u1013493@utah.edu>
2020-05-26 16:17:33 -0600

    change zfp_rate to 32

--- include/saena_matrix.h
+++ include/saena_matrix.h
@@ -196,7 +196,7 @@ public:
                   zfp_send_comp_sz = 0,
                   zfp_recv_buff_sz = 0;
 
-    unsigned zfp_rate      = 64;
+    unsigned zfp_rate      = 32;
     double   zfp_precision = 32;
 
 //    double  *zfp_send_buff = nullptr, // storage for compressed send_stream
