ecce275331ca69638cfbbabe0b5b4d91f5cce2d1
Majid Rasouli <u1013493@utah.edu>
2020-08-21 15:35:32 -0600

    update experiments/lazy

--- experiments/lazy.cpp
+++ experiments/lazy.cpp
@@ -22,6 +22,9 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
+    // set the number of OpenMP threads at run-time
+    omp_set_num_threads(1);
+
     // *************************** Ssetup timing parameters ****************************
 
     std::vector<double> setup_time_loc, solve_time_loc;
@@ -105,13 +108,13 @@ int main(int argc, char* argv[]){
 
     char* Vname(argv[2]);
 //    saena::read_vector_file(rhs_std, A, Vname, comm);
-    read_vector_file(rhs_std, A.get_internal_matrix(), Vname, comm);
+    read_from_file_rhs(rhs_std, A.get_internal_matrix(), Vname, comm);
 
     // set rhs_std
 //    A.get_internal_matrix()->matvec(v, rhs_std);
 //    rhs_std = v;
 
-    index_t my_split;
+    index_t my_split = 0;
     saena::find_split((index_t)rhs_std.size(), my_split, comm);
 
     rhs.set(&rhs_std[0], (index_t)rhs_std.size(), my_split);
@@ -143,17 +146,7 @@ int main(int argc, char* argv[]){
 //    saena::options opts;
     saena::amg solver;
     solver.set_verbose(verbose); // set verbose at the beginning of the main function.
-//    solver.set_multigrid_max_level(0); // 0 means only use direct solver, so no multigrid will be used.
-
-//    if(rank==0) printf("usage: ./Saena x_size y_size z_size sparse_epsilon \n");
-//    double sp_epsilon(std::atof(argv[4]));
-//    if(rank==0) printf("\nsp_epsilon = %f \n", sp_epsilon);
-//    solver.get_object()->sparse_epsilon = sp_epsilon;
-
-    // receive sparsifivation factor from input and set it.
-//    double sm_sz_prct(std::stof(argv[4]));
-//    if(rank==0) printf("sm_sz_prct = %f \n", sm_sz_prct);
-//    solver.set_sample_sz_percent(sm_sz_prct);
+    solver.set_multigrid_max_level(3); // 0 means only use direct solver, so no multigrid will be used.
 
     solver.set_matrix(&A, &opts);
     solver.set_rhs(rhs);
@@ -172,7 +165,7 @@ int main(int argc, char* argv[]){
     t1 = MPI_Wtime();
 
 //    solver.solve(u, &opts);
-    solver.solve_pcg(u, &opts);
+    solver.solve_pCG(u, &opts);
 
     t1 = MPI_Wtime() - t1;
     if(solver.verbose) print_time(t1, "Solve:", comm);
@@ -188,15 +181,15 @@ int main(int argc, char* argv[]){
     std::size_t length = file_name2.copy(file_name3, strlen(file_name)-5, 0);
     file_name3[length] = '\0';
 
-    size_t      extIndex       = file_name2.find_last_of(".");
-    std::string file_extension = file_name2.substr(extIndex+1, 3);
+    size_t      extIndex       = file_name2.find_last_of('.');
+    std::string file_extension = file_name2.substr(extIndex + 1, 3);
 
 //    std::cout << "file name: " << file_name << ", file_name2: " << file_name2 << ", file_name3: " << file_name3 << std::endl;
 
     saena::matrix B (comm);
     int lazy_step = 0;
 
-    int update_method = 3;
+    int update_method = 1;
     if(rank==0) printf("================================================\n\nupdate method: %d\n", update_method);
 
 //    char pause1[10];
@@ -210,7 +203,7 @@ int main(int argc, char* argv[]){
     for(int i = 2; i <= ITER_LAZY; i++){
         std::string file_name_update = file_name3;
         file_name_update            += std::to_string(i);
-        file_name_update            += ".";
+        file_name_update            += '.';
         file_name_update            += file_extension;
 
 //        std::cout << "file_name_update: " << file_name_update << std::endl;
@@ -219,18 +212,18 @@ int main(int argc, char* argv[]){
         t1 = MPI_Wtime();
         if( lazy_step % 2 == 1) {
             A.erase_no_shrink_to_fit();
-            //        A.add_duplicates(true);
+//            A.add_duplicates(true);
 
             A.read_file(file_name_update.c_str());
 
             if(update_method == 1) {
                 A.assemble();
-                solver.update1(&(A)); // update the AMG hierarchy
+                solver.update1(&A); // update the AMG hierarchy
             } else if(update_method == 2) {
                 A.assemble();
                 solver.update2(&(A)); // update the AMG hierarchy
             } else if(update_method == 3) {
-                A.assemble_no_scale();
+                A.assemble(false);
                 solver.update3(&(A)); // update the AMG hierarchy
             } else {
                 printf("Error: Wrong update_method is set! Options: 1, 2, 3\n");
@@ -252,13 +245,13 @@ int main(int argc, char* argv[]){
                 B.assemble();
                 solver.update2(&(B)); // update the AMG hierarchy
             } else if(update_method == 3) {
-                B.assemble_no_scale();
+                B.assemble(false);
                 solver.update3(&(B)); // update the AMG hierarchy
             } else {
                 printf("Error: Wrong update_method is set! Options: 1, 2, 3\n");
             }
 
-            lazy_step++;
+            ++lazy_step;
         }
 
         t1 = MPI_Wtime() - t1;
@@ -274,7 +267,7 @@ int main(int argc, char* argv[]){
 
         MPI_Barrier(comm);
         t1 = MPI_Wtime();
-        solver.solve_pcg(u, &opts);
+        solver.solve_pCG(u, &opts);
         t1 = MPI_Wtime() - t1;
         print_time(t1, "Solve:", comm);
         solve_time_loc.emplace_back(t1);
@@ -337,123 +330,6 @@ int main(int argc, char* argv[]){
     }
 */
 
-    // *************************** matvec on different coarse levels of a matrix ****************************
-/*
-    int matvec_iter = 300;
-    int time_num = 4;
-    std::vector<double> time_e1(time_num, 0); // array for timing matvec
-    std::vector<std::vector<double>> time_total; // array for keeping all levels timings
-//    double average1, average2, average3;
-
-    saena_object* amg = solver.get_object();
-    saena_matrix *B;
-    int levels = amg->max_level;
-
-    // warm-up
-    // -------
-    B = amg->grids[0].A;
-    num_local_row = B->M;
-    rhs_std.resize(num_local_row);
-    u.resize(num_local_row);
-    time_e1.assign(time_e1.size(), 0);
-    for (int i = 0; i < 50; i++) {
-        B->matvec_timing1(rhs_std, u, time_e1);
-        rhs_std.swap(u);
-    }
-
-//    if (rank == 0) std::cout << "\nlocal loop, remote loop and communication (including <<set vSend>>) times of matvec"
-//                                " are being printed for different levels of the multigrid hierarchy:" << std::endl;
-
-    if (rank == 0) printf("\n#####################################\n\n");
-    for(int l = 0; l < levels+1; l++) {
-        if (rank == 0) printf("start level %d of %d \n", l, levels);
-
-        if (amg->grids[l].active) {
-            B = amg->grids[l].A;
-            num_local_row = B->M;
-//            printf("level = %d, num_local_row = %d \n", l, num_local_row);
-            rhs_std.resize(num_local_row);
-            u.resize(num_local_row);
-//            if (rank == 0) printf("level %d of %d step1! \n", l, levels);
-
-            // *************************** matvec1 ****************************
-
-            generate_rhs_old(rhs_std);
-            u.assign(num_local_row, 0);
-            time_e1.assign(time_e1.size(), 0);
-//            printf("rank %d: level %d of %d step3! \n", rank, l, levels);
-
-            MPI_Barrier(B->comm);
-//            t1 = omp_get_wtime();
-            for (int i = 0; i < matvec_iter; i++) {
-                B->matvec_timing1(rhs_std, u, time_e1);
-                rhs_std.swap(u);
-            }
-//            t2 = omp_get_wtime();
-
-//        average1 = print_time(t1/double(matvec_iter), t2/double(matvec_iter), "matvec1:", comm);
-//        if (rank==0) printf("_________________________________\n\n");
-//        if (rank==0) printf("local matvec level %d of %d \n", l, levels);
-//        if (rank==0) std::cout << time_e1[1]/(matvec_iter) << std::endl;
-
-//            if (rank == 0) {
-//              std::cout << "\n1- Saena matvec total time:\n" << (time_e1[0]+time_e1[3])/(matvec_iter) << std::endl;
-//              std::cout << std::endl << "matvec1:" << std::endl;
-//                std::cout << time_e1[1] / matvec_iter << std::endl; // local loop
-//                std::cout << time_e1[2] / matvec_iter << std::endl; // remote loop
-//                std::cout << ( time_e1[0] + time_e1[3] - time_e1[1] - time_e1[2]) / matvec_iter << std::endl; // communication including "set vSend"
-//            }
-
-        }
-        time_total.push_back(time_e1);
-    }
-
-    // *************************** print time results ****************************
-
-    // print on output
-    if(rank==0){
-        std::cout << "\ntime results:\n" << std::endl;
-        std::cout << "level \tlocal \t\tremote \t\tcomm \t\ttotal" << std::endl;
-        for(int i = 0; i < time_total.size(); i++)
-            std::cout << i << "\t"
-                      << time_total[i][1]/matvec_iter << "\t"
-                      << time_total[i][2]/matvec_iter << "\t"
-                      << (time_total[i][0] + time_total[i][3] - time_total[i][1] - time_total[i][2])/matvec_iter << "\t"
-                      << (time_total[i][0] + time_total[i][3])/matvec_iter << std::endl;
-    }
-*/
-/*
-    // wrtie to file
-    if(rank==0){
-
-        if(rank==0) {
-            std::string input_filename_ext = argv[1];
-            size_t extIndex = input_filename_ext.find_last_of(".");
-            std::string file_name = "./shrink_";
-            file_name += input_filename_ext.substr(0, extIndex);
-            file_name += ".txt";
-            std::ofstream outFile(file_name);
-
-            outFile << "average time for " << matvec_iter << " matvec iterations" << std::endl;
-            outFile << "matrix name   = " << argv[1] << "\nprocessors    = " << nprocs << std::endl;
-#pragma omp parallel
-            if (rank == 0 && omp_get_thread_num() == 0)
-                outFile << "OpenMP thread = " << omp_get_num_threads() << std::endl;
-
-            outFile << "\ntime results:\n" << std::endl;
-            outFile << "level \tlocal \tremote \tcomm" << std::endl;
-            for (int i = 0; i < time_total.size(); i++)
-                outFile << i << "\t"
-                          << time_total[i][1] / matvec_iter << "\t"
-                          << time_total[i][2] / matvec_iter << "\t"
-                          << (time_total[i][0] + time_total[i][3] - time_total[i][1] - time_total[i][2]) / matvec_iter
-                          << std::endl;
-
-            outFile.close();
-        }
-    }
-*/
-
     // *************************** test for lazy update functions ****************************
 /*
     saena_matrix* A_saena = A.get_internal_matrix();
@@ -484,64 +360,10 @@ int main(int argc, char* argv[]){
     }
 */
 
-    // *************************** matrix-matrix product ****************************
-/*
-    double matmat_time = 0;
-    int matmat_iter_warmup = 0;
-    int matmat_iter = 1;
-
-//    saena::amg solver;
-//    saena::matrix C(comm);
-
-    // warm-up
-    for(int i = 0; i < matmat_iter_warmup; i++){
-        solver.matmat_ave(&A, &A, matmat_time);
-    }
-
-    matmat_time = 0;
-    for(int i = 0; i < matmat_iter; i++){
-        solver.matmat_ave(&A, &A, matmat_time);
-    }
-
-    if(!rank) printf("\nSaena matmat:\n%f\n", matmat_time / matmat_iter);
-*/
-
-    // *************************** matrix-matrix product ****************************
-/*
-    double matmat_time = 0;
-    int matmat_iter_warmup = 1;
-    int matmat_iter = 1;
-
-    saena::amg solver;
-//    saena::matrix C(comm);
-
-    // warm-up
-    for(int i = 0; i < matmat_iter_warmup; i++){
-        solver.matmat_ave(&A, &A, matmat_time);
-    }
-
-    matmat_time = 0;
-    for(int i = 0; i < matmat_iter; i++){
-        solver.matmat_ave(&A, &A, matmat_time);
-    }
-
-    if(!rank) printf("\nSaena matmat:\n%f\n", matmat_time / matmat_iter);
-
-//    petsc_viewer(A.get_internal_matrix());
-//    petsc_viewer(C.get_internal_matrix());
-//    saena_object *obj1 = solver.get_object();
-
-//    petsc_matmat_ave(A.get_internal_matrix(), A.get_internal_matrix(), matmat_iter);
-    petsc_matmat(A.get_internal_matrix(), A.get_internal_matrix());
-//    petsc_check_matmat(A.get_internal_matrix(), A.get_internal_matrix(), C.get_internal_matrix());
-*/
-
     // *************************** finalize ****************************
 
-//    if(rank==0) dollar::text(std::cout);
-
-//    A.destroy();
-//    solver.destroy();
+    A.destroy();
+    solver.destroy();
     MPI_Finalize();
     return 0;
 }
