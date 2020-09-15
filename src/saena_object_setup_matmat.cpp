c6a315980bde5653707868821983483a52b0b2bf
Majid Rasouli <u1013493@utah.edu>
2020-09-14 18:18:00 -0600

    change matmat_iter

--- src/saena_object_setup_matmat.cpp
+++ src/saena_object_setup_matmat.cpp
@@ -1180,7 +1180,7 @@ int saena_object::matmat(saena_matrix *A, saena_matrix *B, saena_matrix *C, cons
     // use this parameter to time matmat, and print it if (print_timing = true)
     double t_temp = 0, t_matmat_prep = 0, t_matmat = 0, t_matmat_tot = 0;
     // perform matmat multiple times and take the average of them.
-    int matmat_iter_warmup = 2, matmat_iter = 3;
+    int matmat_iter_warmup = 10, matmat_iter = 20;
 
     // =======================================
     // Convert A to CSC
