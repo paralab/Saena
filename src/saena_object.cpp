0fd54f2c1fe0f9b9f3b565755c80719e23a13f90
Majid Rasouli <u1013493@utah.edu>
2020-10-21 15:02:22 -0600

    update find_eig

--- src/saena_object.cpp
+++ src/saena_object.cpp
@@ -609,12 +609,17 @@ int saena_object::scale_vector(std::vector<value_t>& v, std::vector<value_t>& w)
 
 int saena_object::find_eig(saena_matrix& A){
 
-    A.scale_matrix(false);
+    // if the linear system is not scaled, scale the matrix only for computing the eigenvalue that is
+    // being used in chebyshev, since chebyshev uses the preconditioned matrix.
+
+    if(!scale)
+        A.scale_matrix(false);
 
 //    find_eig_Elemental(A);
     find_eig_ietl(A);
 
-    A.scale_back_matrix(false);
+    if(!scale)
+        A.scale_back_matrix(false);
 
 //    A.print_entry(-1, "A");
 
